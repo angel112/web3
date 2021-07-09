f420d39c321deefd2295c508313ca0e043bf85fa
Angel Lakra <angel@ivypods.com>
2021-07-09 16:33:05 +0530

    Added favicon and title

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import HomeComp from "@/containers/HomeComp";
+import Head from 'next/head';
 
 function HomePage() {
-    return <HomeComp />
+    return (
+        <React.Fragment>
+            <Head>
+                <title>web3.js</title>
+                <link rel="shortcut icon" href="/web3_logo.png" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <HomeComp />
+        </React.Fragment>
+    )
 }
 
 export default HomePage;
